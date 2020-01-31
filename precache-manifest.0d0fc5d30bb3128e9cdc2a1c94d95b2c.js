b58f309c14272d51e1fca3e6e3a6018c371f0108
Andrew Stiles <ndrwstls544@gmail.com>
2020-01-31 14:33:11 -0500

    Updates

---
+++ precache-manifest.0d0fc5d30bb3128e9cdc2a1c94d95b2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44e1a60c5a43f8597f9137750d817fc3",
+    "url": "/andrew129/React-Trivia/index.html"
+  },
+  {
+    "revision": "af4da14eef9aee5c4f31",
+    "url": "/andrew129/React-Trivia/static/css/main.ffc0a9db.chunk.css"
+  },
+  {
+    "revision": "e7f7b367aa2060e37b18",
+    "url": "/andrew129/React-Trivia/static/js/2.9075058e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/andrew129/React-Trivia/static/js/2.9075058e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "af4da14eef9aee5c4f31",
+    "url": "/andrew129/React-Trivia/static/js/main.9a098c32.chunk.js"
+  },
+  {
+    "revision": "b5e36a2f8bf8e4ab42b3",
+    "url": "/andrew129/React-Trivia/static/js/runtime-main.f4d22423.js"
+  }
+]);
